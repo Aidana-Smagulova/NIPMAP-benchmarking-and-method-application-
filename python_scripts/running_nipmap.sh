d12c1a9d5a3c54842af6c658177de6d86d0978f5
Aidana-Smagulova <132575492+Aidana-Smagulova@users.noreply.github.com>
2024-12-25 15:47:02 +0100

    Delete python_scripts/running_nipmap.sh

--- python_scripts/running_nipmap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --partition=cpu-single
-#SBATCH --ntasks=1
-#SBATCH --time=3:00:00
-#SBATCH --mem=1000gb
-#SBATCH --export=NONE
-
-SCRIPT_PATH="/gpfs/bwfor/work/ws/hd_vb248-nipmap/NIPMAP/7niches_nipmap.py"
-python3 $SCRIPT_PATH
